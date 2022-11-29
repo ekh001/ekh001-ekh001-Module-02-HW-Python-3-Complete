1ae71d12bcf115490fcc6a7a4d486baa8ef38e25
Liz Hall <lizk.hall@gmail.com>
2022-11-29 15:40:29 -0600

    first commit

---
+++ module2house.py
@@ -0,0 +1,16 @@
+def howBigIsMyHouse(l, w):
+    length = 'l'
+    width = 'w'
+    Area = l*w
+    return Area
+
+def circumferenceCircle():
+    from math import pi as p
+    print("Radius of the circle in question: ")
+    radius = float(input())
+    c = 2*p*radius
+    return c
+
+
+
+
